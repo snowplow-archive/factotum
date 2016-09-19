701763a9f8315e21c5de8f8d2240082eb2f5ed36
Ed Lewis <edward.richard.lewis@gmail.com>
2016-09-19 16:51:08 +0100

    Add CI/CD (closes #69, closes #56)
    CICD using travis, OSX deployments are supported by adding
    an OSX build (which deploys the artifact too). Only build
    artifacts from stable Rust versions will be deployed.
    
    The deployment uses Factotum: the dog food is eaten

---
+++ deploy/tasks/ship_release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ship release using release manager
+
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    SUFFIX="_darwin_x86_64"
+    export PATH=$PATH:/Users/travis/Library/Python/2.7/bin
+else 
+    SUFFIX="_linux_x86_64"
+fi
+
+env RM_SUFFIX=${SUFFIX} release-manager --config deploy/tasks/release_config.yaml --make-version  --make-artifact --upload-artifact
